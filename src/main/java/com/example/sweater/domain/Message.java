c1ccd4b0f952feaaee149590d0c72531a249068d
Afanasyev Andrey <andreyyerdna123@gmail.com>
2021-10-22 20:51:52 +0300

    first commit

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,39 @@
+package com.example.sweater.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "афффтар выпил яду";
+    }
+
+    @Getter
+    @Setter
+    private String text;
+    @Getter
+    @Setter
+    private String tag;
+    @Setter
+    @Getter
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+}
